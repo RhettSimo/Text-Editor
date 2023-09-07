530803cf29e0dda120187edb0c60a5d11d7ca79b
RhettSimo <simonsonrhett1337@gmail.com>
2023-09-07 18:03:17 -0500

    fixed some issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,7 @@ const initdb = async () =>
 export const putDb = async (id, content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
